7e521f3b16356bd5df79885965ecb33046935612
PxlSyl <SMCBeatz@protonmail.com.com>
2023-11-20 14:09:47 +0100

    init

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -2,6 +2,7 @@
 
 import codeSampleMdx from './code-sample.mdx.json' assert { type: 'json' }
 import derivingOlsEstimatorMdx from './deriving-ols-estimator.mdx.json' assert { type: 'json' }
+import elementsMdx from './elements.mdx.json' assert { type: 'json' }
 import githubMarkdownGuideMdx from './github-markdown-guide.mdx.json' assert { type: 'json' }
 import guideToUsingImagesInNextjsMdx from './guide-to-using-images-in-nextjs.mdx.json' assert { type: 'json' }
 import introducingTailwindNextjsStarterBlogMdx from './introducing-tailwind-nextjs-starter-blog.mdx.json' assert { type: 'json' }
@@ -10,6 +11,5 @@ import newFeaturesInV1Mdx from './new-features-in-v1.mdx.json' assert { type: 'j
 import picturesOfCanadaMdx from './pictures-of-canada.mdx.json' assert { type: 'json' }
 import releaseOfTailwindNextjsStarterBlogV2_0Mdx from './release-of-tailwind-nextjs-starter-blog-v2.0.mdx.json' assert { type: 'json' }
 import theTimeMachineMdx from './the-time-machine.mdx.json' assert { type: 'json' }
-import elementsMdx from './elements.mdx.json' assert { type: 'json' }
 
-export const allBlogs = [codeSampleMdx, derivingOlsEstimatorMdx, githubMarkdownGuideMdx, guideToUsingImagesInNextjsMdx, introducingTailwindNextjsStarterBlogMdx, myFancyTitleMdx, newFeaturesInV1Mdx, picturesOfCanadaMdx, releaseOfTailwindNextjsStarterBlogV2_0Mdx, theTimeMachineMdx, elementsMdx]
+export const allBlogs = [codeSampleMdx, derivingOlsEstimatorMdx, elementsMdx, githubMarkdownGuideMdx, guideToUsingImagesInNextjsMdx, introducingTailwindNextjsStarterBlogMdx, myFancyTitleMdx, newFeaturesInV1Mdx, picturesOfCanadaMdx, releaseOfTailwindNextjsStarterBlogV2_0Mdx, theTimeMachineMdx]
