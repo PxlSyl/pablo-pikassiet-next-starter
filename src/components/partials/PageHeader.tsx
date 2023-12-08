3a9ca6509f8f55285c671beaabb7939d005cfbc5
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-08 15:25:51 +0100

    init

--- src/components/partials/PageHeader.tsx
+++ src/components/partials/PageHeader.tsx
@@ -1,16 +1,30 @@
+'use client'
+
 import { humanize } from '@/lib/utils/textConverter'
 import Breadcrumbs from '../blog/Breadcrumbs'
+import { motion } from 'framer-motion'
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: -25 },
+  enter: { opacity: 1, x: 0, y: 0 },
+}
 
 const PageHeader = ({ title }: { title: string }) => {
   return (
-    <section className="mt-20">
+    <motion.section
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      transition={{ type: 'linear' }}
+      className="mt-20"
+    >
       <div className="container text-center">
         <div className="rounded-2xl bg-gradient-to-b from-body to-theme-light px-8 py-14 dark:from-darkmode-body dark:to-darkmode-theme-light">
           <h1 className="text-highlighted dark:text-highlighted">{humanize(title)}</h1>
           <Breadcrumbs className="mt-6" />
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
