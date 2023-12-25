0b5470f7fc5e15d7092058adac040d197ef22c3f
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-25 06:17:01 +0100

    init

--- components/blog/projectsCard.tsx
+++ components/blog/projectsCard.tsx
@@ -1,8 +1,13 @@
+import { Project } from '@/types'
 import { plainify } from '@/lib/utils/textConverter'
 import Image from './Image'
 import Link from './Link'
 
-const ProjectsCard = ({ data }) => {
+type ProjectsCardProps = {
+  data: Project
+}
+
+const ProjectsCard = ({ data }: ProjectsCardProps) => {
   const { title, imgSrc, href } = data.frontmatter
   return (
     <div className="max-w-[544px] p-4">
@@ -11,48 +16,31 @@ const ProjectsCard = ({ data }) => {
           imgSrc && 'h-full'
         }  rounded-2xl bg-gradient-to-b from-body to-theme-light dark:from-darkmode-body dark:to-darkmode-theme-light`}
       >
-        {imgSrc &&
-          (href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              <Image
-                alt={title}
-                src={imgSrc}
-                className="rounded-2xl object-cover object-center md:h-36 lg:h-48"
-                width={544}
-                height={306}
-              />
-            </Link>
-          ) : (
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="rounded-2xl object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          ))}
+        <Link href={href} aria-label={`Link to ${title}`}>
+          <Image
+            alt={title}
+            src={imgSrc}
+            className="rounded-2xl object-cover object-center md:h-36 lg:h-48"
+            width={544}
+            height={306}
+          />
+        </Link>
         <div className="p-6">
           <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-            {href ? (
-              <Link href={href} aria-label={`Link to ${title}`}>
-                {title}
-              </Link>
-            ) : (
-              title
-            )}
+            <Link href={href} aria-label={`Link to ${title}`}>
+              {title}
+            </Link>
           </h2>
           <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">
             {plainify(data.content.slice(0, Number(149)))}...
           </p>
-          {href && (
-            <Link
-              href={href}
-              className="text-base font-medium leading-6 text-highlighted hover:opacity-80 dark:text-darkmode-highlighted"
-              aria-label={`Link to ${title}`}
-            >
-              Learn more &rarr;
-            </Link>
-          )}
+          <Link
+            href={href}
+            className="text-base font-medium leading-6 text-highlighted hover:opacity-80 dark:text-darkmode-highlighted"
+            aria-label={`Link to ${title}`}
+          >
+            Learn more &rarr;
+          </Link>
         </div>
       </div>
     </div>
