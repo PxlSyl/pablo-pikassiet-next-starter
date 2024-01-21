f76f826a25d1cc9c3c0fc31042f600cc81270e57
PxlSyl <SMCBeatz@protonmail.com.com>
2024-01-21 14:07:11 +0100

    fix

--- lib/utils/textConverter.ts
+++ lib/utils/textConverter.ts
@@ -28,8 +28,8 @@ export const humanize = (content: string) => {
 }
 
 // plainify
-export const plainify = (content: string) => {
-  const parseMarkdown = marked.parse(content)
+export const plainify = async (content: string) => {
+  const parseMarkdown = await marked.parse(content) // Wait for the promise to resolve
   const filterBrackets = parseMarkdown.replace(/<\/?[^>]+(>|$)/gm, '')
   const filterSpaces = filterBrackets.replace(/[\r\n]\s*[\r\n]/gm, '')
   const stripHTML = htmlEntityDecoder(filterSpaces)
